e2b761f1c2ead06fa9c7144b126da7bfd7e4af5e
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2023-01-30 11:37:42 +0530

    fix: fix shellcheck errors

--- ci/launch-linux-services.sh
+++ ci/launch-linux-services.sh
@@ -18,5 +18,5 @@
 
 test_type=$1
 
-make ci-env-up project_compose_ci=ci/pod/docker-compose.$test_type.yml
-[ $test_type != "first" ] && sudo ./ci/init-$test_type-test-service.sh
+make ci-env-up project_compose_ci=ci/pod/docker-compose."$test_type".yml
+[ "$test_type" != "first" ] && sudo ./ci/init-"$test_type"-test-service.sh
