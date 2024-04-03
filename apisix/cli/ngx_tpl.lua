9cdb6e5009531ecb884b72d33bf2f86562cd9f01
Nic <qianyong@api7.ai>
2024-04-03 17:26:14 +0800

    fix: add saml_sessions shared dict to ngx_tpl (#315)
    Signed-off-by: Nic <qianyong@api7.ai>

--- apisix/cli/ngx_tpl.lua
+++ apisix/cli/ngx_tpl.lua
@@ -335,6 +335,10 @@ http {
     lua_shared_dict cas_sessions {* http.lua_shared_dict["cas-auth"] *};
     {% end %}
 
+    {% if enabled_plugins["saml-auth"] then %}
+    lua_shared_dict saml_sessions {* http.lua_shared_dict["saml_sessions"] *};
+    {% end %}
+
     {% if enabled_plugins["authz-keycloak"] then %}
     # for authz-keycloak
     lua_shared_dict access-tokens {* http.lua_shared_dict["access-tokens"] *}; # cache for service account access tokens
