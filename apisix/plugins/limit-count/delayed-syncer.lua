e8f3390a0727a76c8e45795e1f3cf5742b7ae226
Nic <qianyong@api7.ai>
2025-01-06 15:29:18 +0800

    fix: local_delta may be nil (#748)
    Signed-off-by: Nic <qianyong@api7.ai>

--- apisix/plugins/limit-count/delayed-syncer.lua
+++ apisix/plugins/limit-count/delayed-syncer.lua
@@ -317,14 +317,16 @@ function _M.sync(self, syncer_id, time_to_sync)
             core.log.error("get local delta from shm failed: ", err)
         end
 
-        _, remaining_or_err, reset = self.limiter:incoming(key, delta)
-        -- compat
-        if type(remaining_or_err) ~= "string" then
-            self:sync_to_shm(key, remaining_or_err, reset, delta)
-        elseif remaining_or_err ~= "rejected" then
-            core.log.error("sync to redis failed: ", remaining_or_err, ", key: ", key)
-        else
-            self:sync_to_shm(key, 0, reset, delta)
+        if delta then
+            _, remaining_or_err, reset = self.limiter:incoming(key, delta)
+            -- compat
+            if type(remaining_or_err) ~= "string" then
+                self:sync_to_shm(key, remaining_or_err, reset, delta)
+            elseif remaining_or_err ~= "rejected" then
+                core.log.error("sync to redis failed: ", remaining_or_err, ", key: ", key)
+            else
+                self:sync_to_shm(key, 0, reset, delta)
+            end
         end
 
         local ok, err_unlock = locker:unlock()
